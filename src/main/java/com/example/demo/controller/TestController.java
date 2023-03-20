7579cd03adcd1089b22c92a6a3c2b44f48b127af
= <=>
2023-03-20 16:04:36 +0900

    3/20일 수업 완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -17,4 +20,43 @@ public class TestController {
         return "Hello World! 김다은 "+ id;
     }
 
+    @GetMapping("/testRequestParam")
+    public  String testControllerRequestParam(@RequestParam(required=false)int id)
+    {
+        return "Hello World! 김다은 "+ id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public  String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO)
+    {
+        return "Hello World! 김다은. id: "+ testRequestBodyDTO.getId() + ", message: "
+                + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody()
+    {
+        List<String> list= new ArrayList<>();
+        list.add("kde");
+        list.add("kde2");
+        list.add("kde33");
+
+        ResponseDTO<String> response=ResponseDTO.<String>builder().data(list).build();
+
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponsEnity()
+    {
+        List<String> list= new ArrayList<>();
+        list.add("kde");
+        list.add("kde2");
+        list.add("kde33");
+
+        ResponseDTO<String> response=ResponseDTO.<String>builder().data(list).build();
+
+        return ResponseEntity.ok().body(response);
+        //return ResponseEntity.badRequest().body(response);
+    }
 }
