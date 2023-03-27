409f996f4931a42e5ffb2b78524c74138f01be61
= <=>
2023-03-27 16:17:19 +0900

    3/27 수업 완료(2.3절 retrieve 까지 완료)

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
 import com.example.demo.dto.TodoDTO;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -17,15 +19,58 @@ public class TodoController {
     @Autowired
     private TodoService service;
 
-    @GetMapping("/test")
-    public ResponseEntity<?> testTodo(){
-        String str= service.testService();
-        List<String> list= new ArrayList<>();
+    /*@GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
         list.add(str);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
+    }*/
 
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+        try {
+            String temporaryUserId = "temporary-user";
+
+            TodoEntity entity= TodoDTO.toEntity(dto);
+
+            entity.setId(null);
+
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.create(entity);
+
+            List<TodoDTO> dtos = entities.stream()
+                        .map(TodoDTO::new)
+                        .collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
+                        .data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+            } catch (Exception e){
+                String error = e.getMessage();
+                ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+                return ResponseEntity.badRequest().body(response);
+        }
     }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList(){
+        String temporaryUserId = "temporary-user";
+
+        List<TodoEntity> entities = service.retrieve(temporaryUserId);
+
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new)
+                .collect(Collectors.toList());
+
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
+                .data(dtos).build();
+
+        return ResponseEntity.ok().body(response);
+    }
+
 
 
 }
--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.persistence;
 
 import com.example.demo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
-    List<TodoEntity> findByUserId(String userId);
+    //@Query(value="select * from ", nativeQuery = true)
+    List<TodoEntity> findByUserId(String userId); //한 사람이 여러 아이디를 쓸 수 있으니까 리스트 사용
 }
